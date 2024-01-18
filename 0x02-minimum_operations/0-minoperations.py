990fe400c3ff1d3f25ffbf185cbc12992e0812cb
Stephen Gitau <stephenpepe.sm@gmail.com>
2024-01-18 12:35:20 -0800

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" The minimum operations """
+"""The minimum operations"""
 
 
 def minOperations(n):
-    """Computes the fewest number of operations needed to result in exactly n H characters."""
+    """Computes the fewest number of operations needed to result in exactly n H"""
     if not isinstance(n, int):
         return 0
     _count = 0
