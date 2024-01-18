f8b8d783e486cbefd85ddb87a9628f2b9b35d932
Stephen Gitau <stephenpepe.sm@gmail.com>
2024-01-18 12:33:50 -0800

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" The minimum operations """
+
+
+def minOperations(n):
+    """Computes the fewest number of operations needed to result in exactly n H characters."""
+    if not isinstance(n, int):
+        return 0
+    _count = 0
+    board = 0
+    done = 1
+    while done < n:
+        if board == 0:
+            board = done
+            done += board
+            _count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            board = done
+            done += board
+            _count += 2
+        elif board > 0:
+            done += board
+            _count += 1
+    return _count
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,6 @@
+trying to answer minumum operation 
+the code should try to evaluate how many times it takes n number of H to be printed out using copy and past, 
+example 
+n=9
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+as in it takes 6 operations
